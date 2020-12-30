58803504ba381da59ee373775b15261da69bc4f2
kiran-code <ravikiransjce.code@gmail.com>
2020-12-30 09:31:35 +0530

    Improved UI to show different types of files

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 function callServer(){
-  console.log('callServer');
   fetchFileManagerData();
 }
 
@@ -11,11 +10,8 @@ async function fetchFileManagerData(entity_id, entity_type){
       mode: 'cors',
       method: 'POST',
     });
-    console.log('file manager data',response);
     const data = await response.json();
-    console.log(data);
     processFilesInfo(data);
-    //return data;
   }catch(err){
     console.error(err);
   }
@@ -26,9 +22,6 @@ function processFilesInfo(data){
   showPath(path);
   const filesInfoArray = data.filesInfo;
   showFilesAndDirectories(filesInfoArray);
-  console.log(path);
-  console.log(filesInfoArray);
-
 }
 
 function showPath(path){
@@ -43,9 +36,37 @@ function showPath(path){
 function showFilesAndDirectories(filesInfoArray){
   const filesAndDirectoriesRow = document.getElementById('filesAndDirectories');
   filesInfoArray.forEach(file => {
+    let icon = './icons/file.png';
+    let directory = '';
+    switch (file.extension) {
+      case ".js":
+        icon = "./icons/javascript.png";
+        break;
+      case ".css":
+        icon = "./icons/css.png";
+        break;
+      case ".html":
+        icon = "./icons/html.png";
+        break;
+      case ".txt":
+        icon = "./icons/txt.png";
+        break;  
+      case "directory":
+        directory = 'directory';
+        icon = "./icons/folder.png";
+        break;
+      default:
+        icon = "./icons/file.png";
+        break;
+    }
     const col = document.createElement('div');
-    col.classList.add('col-md-4');
-    col.innerHTML = file.name;
+    col.classList.add('col-md-2','cardStyle','mt-2');
+    col.innerHTML = `<div class="card bg-dark text-white text-center ${directory}">
+                      <img src="${icon}" class="img-fluid-fm" alt="image not found">
+                      <div class="card-img-overlay cardBackGround">
+                        <p class="card-title text-white" style="padding-top:80%;">${file.name}</p>
+                      </div>
+                    </div>`;
     filesAndDirectoriesRow.append(col);
   })
 }
